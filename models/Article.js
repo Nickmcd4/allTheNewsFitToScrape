1fe2001e80367d331d88f3edd8038bb17f2bcd19
NickMcD4 <nickmcd4@gmail.com>
2018-10-01 17:00:09 -0600

    pls

--- models/Article.js
+++ models/Article.js
@@ -12,6 +12,10 @@ var ArticleSchema = new Schema({
     type: String,
     required: true
   },
+  headline: {
+    type: String,
+    required: true
+  },
   comment: [{
     type: Schema.Types.ObjectId,
     ref: "Comment"
