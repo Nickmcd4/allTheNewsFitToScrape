7ba6344a781d8a55aecf04fa5396db858a51908a
NickMcD4 <nickmcd4@gmail.com>
2018-10-01 15:37:04 -0600

    fixed whatever I pushed previously

---
+++ public/app.js
@@ -0,0 +1,65 @@
+// Grab the articles as a json
+
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+$.getJSON("/articles-json", function(data) {
+    // For each one
+    for (var i = 0; i < data.length; i++) {
+      // Display the apropos information on the page
+    //  console.log("LOOK AT ME" + data[i]._id + data[i].title + data[i].link);
+    }
+  });
+
+
+  var thisId = $("#idCatch").attr("data-id");
+
+  $.ajax({
+      method: "GET",
+      url: "articles/" + thisId
+  })
+
+  .then(function(data){
+      console.log(data);
+
+      for (var i = 0; i < data.length; i++) {
+        // Display the apropos information on the page
+      //  console.log("LOOK AT ME" + data[i]._id + data[i].title + data[i].link);
+     
+        
+      }
+      $("#commentSection").prepend(data.comment[0].title);
+
+      $("#commentSection").prepend(data.comment[0].body);
+  })
+
+
+
+});
+
+$("#submitter").on("click", function(event){
+    event.preventDefault();
+
+    var thisId = $("#idCatch").attr("data-id");
+
+    $.ajax({
+        method: "POST",
+        url: "/articles/" + thisId,
+        data: {
+            title: $("#titleinput").val(),
+
+            body: $('#bodyinput').val()
+        }
+    })
+
+    .then(function(data){
+        console.log(data);
+
+
+    })
+
+    $('#titleinput').val("");
+    $("#bodyinput").val("");
+    console.log(thisId);
+})
--- server.js
+++ server.js
@@ -1,121 +1,45 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var logger = require("morgan");
-var mongoose = require("mongoose");
+//dependencies
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var logger = require('morgan');
 
-var Note = require("./models/Note.js");
-var Article = require("./models/Article.js");
-
-var request = require("request");
-var cheerio = require("cheerio");
-
-var PORT = process.env.PORT || 3000;
-mongoose.Promise = Promise;
-
+//initialize Express app
+var express = require('express');
 var app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-app.use(express.static("public"));
+app.use(express.static(process.cwd() + '/public'));
 
-mongoose.connect("mongodb://heroku_j5jgln1r:kadq0g2imkt7rfqf61t58aeruv@ds163681.mlab.com:63681/heroku_j5jgln1r");
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+
+var databaseUri = 'mongodb://localhost/scraped_news';
+
+if (process.env.MONGODB_URI){
+  mongoose.connect(process.env.MONGOD_URI);
+} else {
+  mongoose.connect(databaseUri);
+}
+
 var db = mongoose.connection;
 
-db.on("error", function(error) {
-  console.log("Mongoose Error: ", error);
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to Mongoose!')
 });
 
-db.once("open", function() {
-  console.log("Mongoose connection successful.");
-});
+var routes = require('./controller/controller.js');
+app.use('/', routes);
 
-app.get("/scrape", function(req, res) {
-  request("https://news.google.com/", function(error, response, html) {
-    var $ = cheerio.load(html);
-    $("h2.esc-lead-article-title").each(function(i, element) {
-      var result = {};
-      result.title = $(this).children("a").children("span").text();
-      result.link = $(this).children("a").attr("href");
-      var entry = new Article(result);
-      entry.save(function(err, doc) {
-        {unique: true}
-        if (err) {
-          console.log(err);
-        }
-        else {
-          console.log(doc);
-        }
-      });
-
-    });
-  });
-  res.redirect("/");
-});
-
-app.get("/articles", function(req, res) {
-  Article.find({}, function(error, doc) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      res.json(doc);
-    }
-  });
-});
-
-app.get("/articles/:id", function(req, res) {
-  Article.findOne({ "_id": req.params.id })
-  .populate("note")
-  .exec(function(error, doc) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      res.json(doc);
-    }
-  });
-});
-
-
-app.post("/articles/:id", function(req, res) {
-  var newNote = new Note(req.body);
-
-  newNote.save(function(error, doc) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
-      .exec(function(err, doc) {
-        if (err) {
-          console.log(err);
-        }
-        else {
-          res.send(doc);
-        }
-      });
-    }
-  });
-});
-//delete is not working
-app.delete("/delete/:id", function (req, res) {
-  var id = req.params.id.toString();
-  Note.remove({
-    "_id": id
-  }).exec(function (error, doc) {
-    if (error) {
-      console.log(error);
-    }
-    else {
-      console.log("note deleted");
-      res.redirect("/" );
-    }
-  });
-});
-
-app.listen(PORT, function() {
-  console.log("App running on port 3000!");
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log('Listening on PORT ' + port);
 });
